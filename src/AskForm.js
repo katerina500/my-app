145ecf680dc6c2b7d3fe13deb84249b0cd51cc37
katerina500 <ekaterina.paramore@yandex.ru>
2024-04-04 14:10:28 +0400

    изменила стили

--- src/AskForm.js
+++ src/AskForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 
 const AscForm = () => {
     const [name, setName] = useState('');
@@ -13,13 +13,13 @@ const AscForm = () => {
   
     return (
       <div>
-        <input style={{width:"250px" ,height: "30px",color:"#070386"}}
+        <input style={{width:"200px" ,height: "30px",color:"#070386", textAlign: "center", borderRadius: "10px"}}
           value = {name}
           onChange={handleChange}
           placeholder={'Введите имя'}
         />
   
-        <button style={{padding: "10px", color: "#fff", background: "#61dafb"}} 
+        <button style={{padding: "10px", color: "#fff", background: "#61dafb", borderRadius: "10px", marginLeft: "30px"}} 
         onClick={handleClick}>Отправить</button>
       </div>
     );
