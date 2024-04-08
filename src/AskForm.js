857cc52fd797e0ca4f5cce36d4c07a6f65d49f4d
katerina500 <ekaterina.paramore@yandex.ru>
2024-04-08 15:23:34 +0400

    +

--- src/AskForm.js
+++ src/AskForm.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 const AscForm = () => {
     const [name, setName] = useState('');
+    const [isActive, setIsActive] = useState(false);
   
     const handleChange = event => {
       setName(event.target.value);
@@ -9,18 +10,27 @@ const AscForm = () => {
   
     const handleClick = () => {
       setName('');
+      setIsActive(current => !current);
     };
+
+    
   
     return (
-      <div>
-        <input style={{width:"200px" ,height: "30px",color:"#070386", textAlign: "center", borderRadius: "10px"}}
+        <div className="box">
+        <input style={{width:"200px" ,height: "30px", textAlign: "center", borderRadius: "10px"}}
           value = {name}
           onChange={handleChange}
           placeholder={'Введите имя'}
         />
   
-        <button style={{padding: "10px", color: "#fff", background: "#61dafb", borderRadius: "10px", marginLeft: "30px"}} 
-        onClick={handleClick}>Отправить</button>
+        <button className="btn" style={{
+          marginLeft: "30px",
+          backgroundColor: isActive ? 'black' : '',
+          color: isActive ? 'white' : '',
+        }} 
+        onClick={handleClick}
+        >Отправить
+          </button>
       </div>
     );
   };
