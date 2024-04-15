8342002fecad7e3335369a9ef9ec9a3fb978bf54
katerina500 <ekaterina.paramore@yandex.ru>
2024-04-15 08:40:34 +0400

    ++

--- src/AskForm.js
+++ src/AskForm.js
@@ -1,38 +1,50 @@
 import React, {useState} from "react";
 
-const AscForm = () => {
-    const [name, setName] = useState('');
-    const [isActive, setIsActive] = useState(false);
+
+  const AscForm = () => {
+    const [name, setName] = useState("");
+    const [isEvenClick, setIsEvenClick] = useState(false);
   
-    const handleChange = event => {
+    const handleChange = (event) => {
       setName(event.target.value);
     };
   
     const handleClick = () => {
-      setName('');
-      setIsActive(current => !current);
+      setName("");
+      setIsEvenClick((current) => !current);
     };
-
-    
   
     return (
-        <div className="box">
-        <input style={{width:"200px" ,height: "30px", textAlign: "center", borderRadius: "10px"}}
-          value = {name}
+      <div
+        className="box"
+        style={{
+          backgroundColor: isEvenClick ? "coral" : "initial",
+        }}
+      >
+        <input
+          style={{
+            width: "200px",
+            height: "30px",
+            textAlign: "center",
+            borderRadius: "10px",
+          }}
+          value={name}
           onChange={handleChange}
-          placeholder={'Введите имя'}
+          placeholder={"Введите имя"}
         />
   
-        <button className="btn" style={{
-          marginLeft: "30px",
-          backgroundColor: isActive ? 'black' : '',
-          color: isActive ? 'white' : '',
-        }} 
-        onClick={handleClick}
-        >Отправить
-          </button>
+        <button
+          className="btn"
+          style={{
+            backgroundColor: isEvenClick ? "green" : "red",
+          }}
+          onClick={handleClick}
+        >
+          Отправить
+        </button>
       </div>
     );
   };
+  
 
 export default AscForm;
